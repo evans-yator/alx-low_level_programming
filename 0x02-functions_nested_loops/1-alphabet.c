2c41413819a43e3edcfd7e3b60c4bef13ccebda4
Evans Yator <kevansyator@gmail.com>
2022-09-13 06:49:03 -0700

    added mandatory files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *Write a function that prints the alphabet, in lowercase, followed by a new line.
+ * Return: always 0;
+ *
+ */
+void print_alphabet(void);
+{
+	char i;
+
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
